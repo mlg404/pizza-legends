99659fa4e623d76c5943f6fa2d96cdd85725e548
Victor Eyer <eyervictor@gmail.com>
2022-10-05 01:46:34 -0300

    refactor: add street map

---
+++ maps/street.ts
@@ -0,0 +1,31 @@
+import { asGridCoords, DirectionsEnum, withGrid } from "../utils";
+import { ConfigObjectType, EventType, Map, MapsEnum } from "./interfaces";
+
+export const Street: Map = {
+  id: "Street",
+  lowerSrc: "./images/maps/StreetLower.png",
+  upperSrc: "./images/maps/StreetUpper.png",
+  configObjects: {
+    hero: {
+      type: ConfigObjectType.PERSON,
+      x: withGrid(30),
+      y: withGrid(10),
+      isPlayerControlled: true,
+    },
+  },
+  cutsceneSpaces: {
+    [asGridCoords(29, 9)]: [
+      {
+        events: [
+          {
+            type: EventType.CHANGE_MAP,
+            map: MapsEnum.KITCHEN,
+            x: withGrid(5),
+            y: withGrid(10),
+            direction: DirectionsEnum.UP,
+          },
+        ],
+      },
+    ],
+  },
+};
