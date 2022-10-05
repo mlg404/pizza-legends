638f46f75cb8e8a544371738373ff74c5569d393
Victor Eyer <eyervictor@gmail.com>
2022-10-05 01:46:42 -0300

    refactor: add kitchen map

---
+++ maps/kitchen.ts
@@ -0,0 +1,43 @@
+import { asGridCoords, DirectionsEnum, withGrid } from "../utils";
+import { ConfigObjectType, EventType, Map, MapsEnum } from "./interfaces";
+
+export const Kitchen: Map = {
+  id: "Kitchen",
+  lowerSrc: "./images/maps/KitchenLower.png",
+  upperSrc: "./images/maps/KitchenUpper.png",
+  configObjects: {
+    hero: {
+      type: ConfigObjectType.PERSON,
+      isPlayerControlled: true,
+      x: withGrid(5),
+      y: withGrid(5),
+    },
+    npcA: {
+      type: ConfigObjectType.PERSON,
+      x: withGrid(9),
+      y: withGrid(6),
+      src: "./images/characters/people/npc2.png",
+    },
+    npcB: {
+      type: ConfigObjectType.PERSON,
+      x: withGrid(10),
+      y: withGrid(8),
+      src: "./images/characters/people/npc3.png",
+    },
+  },
+  cutsceneSpaces: {
+    [asGridCoords(5, 10)]: [
+      {
+        events: [
+          {
+            type: EventType.CHANGE_MAP,
+            map: MapsEnum.STREET,
+            x: withGrid(29),
+            y: withGrid(9),
+            direction: DirectionsEnum.DOWN,
+          },
+        ],
+      },
+    ],
+  },
+};
