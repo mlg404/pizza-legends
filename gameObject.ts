1404ba57aade3ab909edb0b5bf6b681ea67acb72
Victor Eyer <eyervictor@gmail.com>
2022-10-05 00:45:49 -0300

    refactor: gameObject in typescript

--- gameObject.ts
+++ gameObject.ts
@@ -1,58 +1,70 @@
-class GameObject {
+import { Sprite } from "./sprite";
+import { DirectionsEnum } from "./utils";
+
+export class GameObject {
+  public id;
+  public isMounted: boolean;
+  public x: number;
+  public y: number;
+  public direction: DirectionsEnum;
+  public sprite;
+  public behaviorLoop;
+  public behavioLoopIndex;
+  public talking;
+  public retryTimeout: number | null;
+
   constructor(config) {
-    this.id = null
-    this.isMounted = false
+    this.id = null;
+    this.isMounted = false;
     this.x = config.x || 0;
     this.y = config.y || 0;
-    this.direction = config.direction || 'down';
+    this.direction = config.direction || "down";
     this.sprite = new Sprite({
       gameObject: this,
-      src: config.src || './images/characters/people/hero.png',
+      src: config.src || "./images/characters/people/hero.png",
     });
 
-    this.behaviorLoop = config.behaviorLoop || []
-    this.behavioLoopIndex = 0
+    this.behaviorLoop = config.behaviorLoop || [];
+    this.behavioLoopIndex = 0;
 
-    this.talking = config.talking || []
+    this.talking = config.talking || [];
   }
 
   mount(map) {
-    this.isMounted = true
+    this.isMounted = true;
 
-    setTimeout(() => {
-      this.doBehaviorEvent(map)
-    }, 1000)
+    window.setTimeout(() => {
+      this.doBehaviorEvent(map);
+    }, 1000);
   }
 
-  update() {
-
-  }
+  update() {}
 
   async doBehaviorEvent(map) {
-
     if (this.behaviorLoop.length === 0) {
       return;
     }
 
     if (map.isCutscenePlaying) {
       if (this.retryTimeout) {
-        clearTimeout(this.retryTimeout);
+        window.clearTimeout(this.retryTimeout);
       }
-      this.retryTimeout = setTimeout(() => {
+      this.retryTimeout = window.setTimeout(() => {
         this.doBehaviorEvent(map);
-      }, 1000)
+      }, 1000);
       return;
     }
 
-    let eventConfig = this.behaviorLoop[this.behavioLoopIndex]
-    eventConfig.who = this.id
+    let eventConfig = this.behaviorLoop[this.behavioLoopIndex];
+    eventConfig.who = this.id;
 
-    const eventHandler = new OverworldEvent({ map, event: eventConfig })
-    await eventHandler.init()
+    const eventHandler = new OverworldEvent({ map, event: eventConfig });
+    await eventHandler.init();
 
-    this.behavioLoopIndex += 1
-    if (this.behavioLoopIndex === this.behaviorLoop.length) this.behavioLoopIndex = 0
+    this.behavioLoopIndex += 1;
+    if (this.behavioLoopIndex === this.behaviorLoop.length)
+      this.behavioLoopIndex = 0;
 
-    this.doBehaviorEvent(map)
+    this.doBehaviorEvent(map);
   }
 }
